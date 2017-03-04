8e95c16508bb92c2cff1ae27f844c0cac9326ca1
DIU <yeaomoon@gmail.com>
2017-03-04 17:23:39 +0600

    main class

---
+++ src/learn/diu/java/Main.java
@@ -0,0 +1,10 @@
+package learn.diu.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Demo demo = new Demo();
+        System.out.println("Hello World");
+    }
+}
